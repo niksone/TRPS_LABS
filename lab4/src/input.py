be087998538bc63b0e84138835b5cb7d41b1a9f9
Krekoten <nikita.krekoten@codex-soft.com>
2022-11-30 00:07:33 +0200

    remove extra lines

--- lab4/src/input.py
+++ lab4/src/input.py
@@ -4,11 +4,8 @@ def read_lines(filepath):
     with open(filepath, 'r') as file:
         data = file.read()
     return data.split('\n')
-
-
 def parse_country(line):
     args = line.split(' ')
-
     country = {
         "name": args[0],
         "ll": {
@@ -21,18 +18,14 @@ def parse_country(line):
         }
     }
     return country
-
-
 def parse_input():
     cases = []
-
     lines = read_lines('./data/input.txt')
     line_index = 0
     case = 0
     while line_index < len(lines):
         counties_len = int(lines[line_index])
         line_index += 1
-
         countries_list = []
         for j in range(counties_len):
             parsed = parse_country(lines[line_index])
@@ -40,5 +33,4 @@ def parse_input():
             line_index += 1
         case += 1
         cases.append(countries_list)
-
     return cases
